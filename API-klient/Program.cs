e9fc9c11591cf04618554ff3ad66bd2b7ecedef0
Te22e-Grufman-Axel <axel.grufman@elev.ga.ntig.se>
2025-01-16 10:06:28 +0100

    First

--- API-klient/Program.cs
+++ API-klient/Program.cs
@@ -9,9 +9,9 @@
 // client1.BaseAddress = new("https://swapi.py4e.com/api/starships/");
 // client2.BaseAddress = new("https://swapi.py4e.com/api/starships/");
 
-// HttpResponseMessage response1 = client1.GetAsync("9/").Result;
-// string playership = (response1.Content.ReadAsStringAsync().Result);
-// Ship_1 S1 = JsonSerializer.Deserialize<Ship_1>(playership);
+// // HttpResponseMessage response1 = client1.GetAsync("9/").Result;
+// // string playership = (response1.Content.ReadAsStringAsync().Result);
+// // Ship_1 S1 = JsonSerializer.Deserialize<Ship_1>(playership);
 
 // HttpResponseMessage response2 = client2.GetAsync("10/").Result;
 // string Enemyship = (response2.Content.ReadAsStringAsync().Result);
@@ -23,8 +23,8 @@
 // // costofenemyship = ParseCost(S2.cost_in_credits);
 
 // // Console.WriteLine(costofenemyship + " : " + S1.name);
-// // Console.WriteLine(costofyourship + " : " + S2.name);
-// if(costofenemyship > costofyourship)
+// Console.WriteLine(costofyourship + " : " + S2.name);
+// if (costofenemyship > costofyourship)
 // {
 //     Console.WriteLine("Enemy wins");
 // }
@@ -33,28 +33,18 @@
 //     Console.WriteLine("You win");
 // }
 
+// // static int ParseCost(string cost)
+// // {
+// //     bool lyckad = false;
+// //     int resultat = 0;
 
-while (true)
-{
-    Console.WriteLine("Ange en kostnad:");
-    int test = ParseCost(Console.ReadLine());
-    Console.WriteLine($"Du angav: {test}");
-    // Console.ReadLine();
+// //     while (!lyckad)
+// //     {
+// //         lyckad = int.TryParse(cost, out resultat);
+// //     }
 
-
-    static int ParseCost(string cost)
-    {
-        bool lyckad = false;
-        int resultat = 0;
-
-        while (!lyckad)
-        {
-            lyckad = int.TryParse(cost, out resultat);
-        }
-
-        return resultat; 
-    }
+// //     return resultat;
+// // }
 
 
 
-}
--- API-klient/obj/Debug/net8.0/API-klient.AssemblyInfo.cs
+++ API-klient/obj/Debug/net8.0/API-klient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API-klient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a4cb1a8a9370154e5a520c62f2524ec594c7e04")]
 [assembly: System.Reflection.AssemblyProductAttribute("API-klient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API-klient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
