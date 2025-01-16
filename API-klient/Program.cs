837dd96f65c3b83567cb2002c20a20e06de6ceb3
Te22e-Grufman-Axel <axel.grufman@elev.ga.ntig.se>
2025-01-16 13:52:28 +0100

    done

--- API-klient/Program.cs
+++ API-klient/Program.cs
@@ -1,50 +1,39 @@
-﻿// using System.Text.Json;
+﻿using System.Text.Json;
 
-// int costofyourship;
-// int costofenemyship;
+int costofyourship;
+int costofenemyship;
 
-// HttpClient client1 = new();
-// HttpClient client2 = new();
+HttpClient client1 = new();
 
-// client1.BaseAddress = new("https://swapi.py4e.com/api/starships/");
-// client2.BaseAddress = new("https://swapi.py4e.com/api/starships/");
+client1.BaseAddress = new("https://swapi.py4e.com/api/starships/");
 
-// // HttpResponseMessage response1 = client1.GetAsync("9/").Result;
-// // string playership = (response1.Content.ReadAsStringAsync().Result);
-// // Ship_1 S1 = JsonSerializer.Deserialize<Ship_1>(playership);
-
-// HttpResponseMessage response2 = client2.GetAsync("10/").Result;
-// string Enemyship = (response2.Content.ReadAsStringAsync().Result);
-// Ship_2 S2 = JsonSerializer.Deserialize<Ship_2>(Enemyship);
-
-// Console.WriteLine(S2.cost_in_credits + "  Before parse");
-
-// // costofyourship = ParseCost(S1.cost_in_credits);
-// // costofenemyship = ParseCost(S2.cost_in_credits);
-
-// // Console.WriteLine(costofenemyship + " : " + S1.name);
-// Console.WriteLine(costofyourship + " : " + S2.name);
-// if (costofenemyship > costofyourship)
-// {
-//     Console.WriteLine("Enemy wins");
-// }
-// else
-// {
-//     Console.WriteLine("You win");
-// }
-
-// // static int ParseCost(string cost)
-// // {
-// //     bool lyckad = false;
-// //     int resultat = 0;
-
-// //     while (!lyckad)
-// //     {
-// //         lyckad = int.TryParse(cost, out resultat);
-// //     }
-
-// //     return resultat;
-// // }
+HttpResponseMessage response1 = client1.GetAsync("9/").Result;
+string playership = (response1.Content.ReadAsStringAsync().Result);
+Ship_1 S1 = JsonSerializer.Deserialize<Ship_1>(playership);
 
 
+costofyourship = ParseCost(S1.cost_in_credits);
 
+Console.WriteLine(costofyourship + " : " + S1.name);
+Console.ReadLine();
+
+static int ParseCost(string cost)
+{
+    bool lyckad = false;
+    int resultat = 0;
+
+    while (!lyckad)
+    {
+        lyckad = int.TryParse(cost, out resultat);
+    }
+
+    return resultat;
+}
+
+
+  
+public class Ship_1
+{
+    public string name { get; set; }
+    public string cost_in_credits { get; set; }
+}
